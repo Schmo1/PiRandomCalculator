d5df7e00c0de67197a95220ba1d637ac7e38df0e
David <David@DESKTOP-G2>
2023-03-20 21:33:01 +0100

    Projektdateien hinzufügen.

---
+++ PiRandomCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+namespace PiRandomCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type some number to calculate Pi with random numbers");
+            Console.WriteLine("A higher number gets a higher accuration!");
+
+            
+            bool inputValid = int.TryParse( Console.ReadLine(), out int input);
+
+            if(inputValid )
+            {
+                Console.WriteLine(RandomPi(input)); ;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input!");
+            }
+            Console.ReadKey();
+
+        }
+
+
+        public static double RandomPi(int accuration)
+        {
+            Random random = new Random();
+
+            double circle = 0;
+            double square = 0;
+
+            if(accuration == 0) { throw new ArgumentException("Parameter cannot be zero", nameof(accuration)); }
+
+            for (int i = 0; i < accuration; i++)
+            {
+                double[] randomPoint = { random.NextDouble(), random.NextDouble() };
+                double d = Math.Sqrt(Math.Pow(randomPoint[0], 2.0) + Math.Pow(randomPoint[1], 2.0));
+
+                if (d < 1.0)
+                {
+                    circle++;
+                }
+                square++;
+
+            }
+
+            return 4 * circle/square;
+
+        }
+    }
+}
